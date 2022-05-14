c4fabc132d688fd275f060769e5b3e9184a7cdde
Luke Brouwer <53581714+lukebrouwer2000@users.noreply.github.com>
2022-05-14 13:53:10 -0700

    Delete BankTellerView.sql

--- SQLCode/BankTellerView.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW TellerView AS 
-(
-SELECT 
-User.userID, User.emailAddress, User.memberStartDate, UserBalance.balance
-FROM User
-JOIN UserBalance ON User.userID = UserBalance.userID
-);
